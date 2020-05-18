9c7080773a2eb9b3aaae3f6975b366b98417ca84
techelectronick <tech.electro.nick@gmail.com>
2020-05-18 17:58:17 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        UDP udp = new UDP("192.168.0.187", 4210);
+
+        public Form1()
+        {
+            InitializeComponent();
+            getServerDataOnInterval();
+        }
+
+        private void button1_Click(object s, EventArgs e)
+        {
+            udp.Send("power", 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            udp.Send("power", 0);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            TrackBar track = sender as TrackBar;
+            udp.Send("brightness", track.Value);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox box = sender as ComboBox;
+            int index = box.SelectedIndex + 1;
+            udp.Send("mode", index);
+        }
+
+        void getServerDataOnInterval()
+        {
+            Timer timer;
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(TimerEventProcessor);
+            timer.Start();
+        }
+
+        private void TimerEventProcessor(object sender, EventArgs e)
+        {
+            ServerData serverData = udp.GetServerData();
+            if(serverData.power)
+            {
+                label4.Text = "On";
+            } else
+            {
+                label4.Text = "Off";
+            }
+        }
+
+        
+    }
+}
---
+++ UDP.cs
@@ -0,0 +1,152 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    // Тут указываем переменные и тип данных, которые приходят с ESP.
+    class ServerData
+    {
+        public bool power = false;
+    }
+    class UDP
+    {
+
+        private String IP = "";
+        private int PORT = 0;
+
+        // Тут передаем айпи и порт при инициализации конструктора.
+        public UDP(String ip, int port)
+        {
+            this.IP = ip;
+            this.PORT = port;
+        }
+
+
+        // Отправка данных на ESP в виде JSON данных, а именно строку.
+        // Используется конструкция using котрая просто чистит данные.
+        // try catch finally используется для того что бы при случаи ошибки программа не вылетала.
+        // try - тут исполняется код: в случае ошибки исполняется блок catch.
+        // catch - тут выводим ошибку или выполняем код: который требуется выполнить в случае ошибки.
+        // finnaly - этот блок выполняется всегда. В нем просто закрываем соединение.
+        public void Send(String key, String value)
+        {
+            using (UdpClient client = new UdpClient())
+            {
+                try
+                {
+                    client.Connect(IP, PORT);
+                    string message = "{key:'" + key + "', value: '" + value + "'}";
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    int numberOfSentBytes = client.Send(data, data.Length);
+                }
+                catch (Exception e) { }
+                finally
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        // Отправка данных на ESP в виде JSON данных, а именно число.
+        // Используется конструкция using котрая просто чистит данные.
+        // try catch finally используется для того что бы при случаи ошибки программа не вылетала.
+        // try - тут исполняется код: в случае ошибки исполняется блок catch.
+        // catch - тут выводим ошибку или выполняем код: который требуется выполнить в случае ошибки.
+        // finnaly - этот блок выполняется всегда. В нем просто закрываем соединение.
+        public void Send(String key, int value)
+        {
+            using (UdpClient client = new UdpClient())
+            {
+                try
+                {
+                    client.Connect(IP, PORT);
+                    string message = "{key:'" + key + "', value: " + value + "}";
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    int numberOfSentBytes = client.Send(data, data.Length);
+                }
+                catch (Exception e) { }
+                finally
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        // Получаем данные из ESP
+        // Используется конструкция using котрая просто чистит данные.
+        // try catch finally используется для того что бы при случаи ошибки программа не вылетала.
+        // try - тут исполняется код: в случае ошибки исполняется блок catch.
+        // catch - тут выводим ошибку или выполняем код: который требуется выполнить в случае ошибки.
+        // finnaly - этот блок выполняется всегда. В нем просто закрываем соединение.
+        // Если вам присылается обрезанная строка из ESP то увеличте new byte[1024]; до нужного вам размера.
+        // Если данные не присылаются то можно попробовать увеличить вермя ответа сервера server.SendTimeout = 1000; и server.ReceiveTimeout = 1000; 
+        // 1000 - 1 секунда.
+        public String GetData(String key)
+        {
+            byte[] data = Encoding.UTF8.GetBytes("{key:'" + key + "', value: ''}");
+            String output = "";
+            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                try
+                {
+                    server.SendTimeout = 1000;
+                    server.ReceiveTimeout = 1000;
+                    server.SendTo(data, data.Length, SocketFlags.None, new IPEndPoint(IPAddress.Parse(IP), PORT));
+                    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                    EndPoint Remote = (EndPoint)sender;
+                    data = new byte[1024];
+                    int receivedDataLength = server.ReceiveFrom(data, ref Remote);
+                    output = Encoding.UTF8.GetString(data, 0, receivedDataLength);
+                }
+                catch (Exception e) { }
+                finally
+                {
+                    server.Close();
+                }
+            }
+            return output;
+        }
+
+        // Получаем данные из ESP в виде JSON данных.
+        // Используется конструкция using котрая просто чистит данные.
+        // try catch finally используется для того что бы при случаи ошибки программа не вылетала.
+        // try - тут исполняется код: в случае ошибки исполняется блок catch.
+        // catch - тут выводим ошибку или выполняем код: который требуется выполнить в случае ошибки.
+        // finnaly - этот блок выполняется всегда. В нем просто закрываем соединение.
+        // Если вам присылается обрезанная строка из ESP то увеличте new byte[1024]; до нужного вам размера.
+        // Если данные не присылаются то можно попробовать увеличить вермя ответа сервера server.SendTimeout = 1000; и server.ReceiveTimeout = 1000; 
+        // 1000 - 1 секунда.
+        public ServerData GetServerData()
+        {
+            ServerData serverData = new ServerData();
+            byte[] data = Encoding.UTF8.GetBytes("{key:'getPowerState', value: ''}");
+            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                try
+                {
+                    server.SendTimeout = 1000;
+                    server.ReceiveTimeout = 1000;
+                    server.SendTo(data, data.Length, SocketFlags.None, new IPEndPoint(IPAddress.Parse(IP), PORT));
+                    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                    EndPoint Remote = (EndPoint)sender;
+                    data = new byte[1024];
+                    int receivedDataLength = server.ReceiveFrom(data, ref Remote);
+                    
+                    // Тут просто преобразум строку json в Модель или Обьект данных ServerData
+                    serverData = JsonConvert.DeserializeObject<ServerData>(Encoding.UTF8.GetString(data, 0, receivedDataLength));
+                } 
+                catch(Exception e) { } 
+                finally
+                {
+                    server.Close();
+                }
+            }
+            return serverData;
+        }
+    }
+
+    
+}
